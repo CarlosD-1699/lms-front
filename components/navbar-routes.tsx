d293d8bdc1530ba154fc3dab0e28f15dc79f3eb5
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-09-20 15:05:17 -0500

    feat: added sidebar and mobile sidebar

---
+++ components/navbar-routes.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { Ghost, LogOut } from "lucide-react";
+import Link from "next/link";
+
+const NavbarRoutes = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
+  return (
+    <div className="flex gap-x-2 ml-auto">
+      {isTeacherPage || isPlayerPage ? (
+        <Button>
+          <LogOut className="h-4 w-4 mr-2" />
+          Salir
+        </Button>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button size="sm" variant="ghost">
+            Profesor
+          </Button>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default NavbarRoutes;
