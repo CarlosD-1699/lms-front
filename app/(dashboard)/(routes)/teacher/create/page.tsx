816c01a83146088bcc51b6dbd54a44b7ddad628b
CarlosD-1699 <carlos.dbarbosa0716@gmail.com>
2023-09-22 09:13:45 -0500

    feat: added the navbar and a dynamic sidebar

---
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const CreatePage = () => {
+    return ( <div></div> );
+}
+ 
+export default CreatePage;
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -15,10 +15,12 @@ const NavbarRoutes = () => {
   return (
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
-        <Button>
-          <LogOut className="h-4 w-4 mr-2" />
-          Salir
-        </Button>
+        <Link href="/">
+          <Button>
+            <LogOut className="h-4 w-4 mr-2" />
+            Salir
+          </Button>
+        </Link>
       ) : (
         <Link href="/teacher/courses">
           <Button size="sm" variant="ghost">
